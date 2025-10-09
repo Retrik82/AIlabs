eb4e252670f79cc7901bfaa9f0130507fd242dfe
Retrik82 <6549701k2.0@gmail.com>
2025-10-10 02:41:24 +0300

    First lab done

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from sklearn.preprocessing import MinMaxScaler
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('сейчас окружение не ругается)))')
+df = pd.read_csv("test.csv")
+print('--------------------------------------------------------------------------')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(df.head(10))
+print('--------------------------------------------------------------------------')
+df.info()
+print('---------------------------------------------------')
+types = df.dtypes
+print(types)
+print('---------------------------------------------------')
+print('Пропущенные значения')
+is_missing = df.isnull().sum()
+print(is_missing)
+print('---------------------------------------------------')
+home_mod = df["HomePlanet"].mode()[0]
+cryo_sleep_mod = df["CryoSleep"].mode()[0]
+cabin_mod = df["Cabin"].mode()[0]
+destination_mod = df["Destination"].mode()[0]
+vip_mod = df["VIP"].mode()[0]
+name_mod = df["Name"].mode()[0]
+
+age_med = df["Age"].median()
+room_service_med = df["RoomService"].median()
+food_court_med = df["FoodCourt"].median()
+shopping_mall_med = df["ShoppingMall"].median()
+spa_med = df["Spa"].median()
+vr_deck_med = df["VRDeck"].median()
+
+
+df["HomePlanet"] = df["HomePlanet"].fillna(home_mod)
+df["CryoSleep"] = df["CryoSleep"].fillna(cryo_sleep_mod)
+df["Cabin"] = df["Cabin"].fillna(cabin_mod)
+df["Destination"] = df["Destination"].fillna(destination_mod)
+df["VIP"] = df["VIP"].fillna(vip_mod)
+df["Name"] = df["Name"].fillna(name_mod)
+
+df["Age"] = df["Age"].fillna(age_med)
+df["RoomService"] = df["RoomService"].fillna(room_service_med)
+df["FoodCourt"] = df["FoodCourt"].fillna(food_court_med)
+df["ShoppingMall"] = df["ShoppingMall"].fillna(shopping_mall_med)
+df["Spa"] = df["Spa"].fillna(spa_med)
+df["VRDeck"] = df["VRDeck"].fillna(vr_deck_med)
+
+print('---------------------------------------------------')
+print('Пропущенные значения')
+missing_values = df.isnull().sum()
+print(missing_values)
+
+print('---------------------------------------------------')
+scaler = MinMaxScaler()
+
+df["Age"] = scaler.fit_transform(df[["Age"]])
+df["RoomService"] = scaler.fit_transform(df[["RoomService"]])
+df["FoodCourt"] = scaler.fit_transform(df[["FoodCourt"]])
+df["ShoppingMall"] = scaler.fit_transform(df[["ShoppingMall"]])
+df["Spa"] = scaler.fit_transform(df[["Spa"]])
+df["VRDeck"] = scaler.fit_transform(df[["VRDeck"]])
+
+print('---------------------------------------------------')
+columns_to_transform = ["Destination", "HomePlanet"]
+df = pd.get_dummies(df, columns=columns_to_transform)
+
+print('---------------------------------------------------')
+print(df.head(10))
+
+df.to_csv("processed_test.csv", index=False)
+

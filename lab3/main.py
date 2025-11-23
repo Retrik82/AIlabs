34b79d5ff70ae34172ebb2cf5d8cb53a9093aef9
Retrik82 <6549701k2.0@gmail.com>
2025-11-24 01:12:55 +0300

    Chenged by second dataset

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,3 @@
-#Laba3 start
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -10,24 +9,35 @@ from sklearn.metrics import (mean_squared_error,
                              mean_absolute_error,
                              auc,
                              roc_curve)
-
-df = pd.read_csv("data/processed_test.csv")
-
-print(df.head(10))
-
+from sklearn.metrics import r2_score
 
 #Regression
 
-X_cols_1 = ["FoodCourt", "ShoppingMall", "Spa", "VRDeck"]
-y_col_1 = "Age"
+df = pd.read_csv(r"C:\Users\Artsiom\PycharmProjects\AIlabs\data_all\processed_data.csv")
+df.head(10)
+
+df["bmi_smoker"] = df["bmi"] * df["smoker"]
+df["age2"] = df["age"] ** 2
+df["obese"] = (df["bmi"] > 30).astype(int)
+df["has_children"] = (df["children"] > 0).astype(int)
+
+X_cols_1 = ["age", "age2", 'bmi', "obese", "smoker", "has_children",  'children', 'region_northeast', 'region_northwest', 'region_southeast', 'region_southwest', 'bmi_smoker']
+y_col_1 = "charges"
+
+print(df[y_col_1])
 
 X_regression = df[X_cols_1]
 y_regression = df[y_col_1]
 
+
 X_train_r, X_test_r, y_train_r, y_test_r = train_test_split(
     X_regression, y_regression, test_size = 0.3, random_state = 42)
 
-regression_tree_model = DecisionTreeRegressor(max_depth = 4, random_state = 42)
+regression_tree_model = DecisionTreeRegressor(
+    max_depth=7,
+    min_samples_split=20,
+    min_samples_leaf=5,
+    random_state=42)
 regression_tree_model.fit(X_train_r, y_train_r)
 
 y_predict_test_r = regression_tree_model.predict(X_test_r)
@@ -40,18 +50,20 @@ print('Mean Square Error: ', MSE)
 print('Root MSE: ', RMSE)
 print('Mean Absolute Error: ', MAE)
 
+R2 = r2_score(y_test_r, y_predict_test_r)
+print('R-squared: ', R2)
+
 plt.figure(figsize = (10, 6))
 plot_tree(regression_tree_model, filled = True, feature_names = X_regression.columns)
-plt.title("Дерево решений (Регрессия Age)")
+plt.title("Дерево решений (Регрессия bmi)")
 plt.show()
 
-#classification
-
-X_cols_2 = ["Destination_55 Cancri e", "Destination_PSO J318.5-22",
-          "Destination_TRAPPIST-1e", "HomePlanet_Earth",
-          "HomePlanet_Europa", "HomePlanet_Mars", "FoodCourt",
-          "Spa"]
-y_col_2 = "VIP"
+X_cols_2 = ["age", "bmi", "children", "charges",
+    "region_northeast", "region_northwest",
+    "region_southeast", "region_southwest",
+    "sex_female", "sex_male"
+]
+y_col_2 = "smoker"
 
 X_classification = df[X_cols_2]
 y_classification = df[y_col_2]
@@ -59,13 +71,15 @@ y_classification = df[y_col_2]
 X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(
     X_classification, y_classification, test_size = 0.3, random_state = 42)
 
-classification_tree_model = DecisionTreeClassifier(max_depth=4, max_leaf_nodes = 10, random_state = 42)
+classification_tree_model = DecisionTreeClassifier(
+    max_depth=6,
+    min_samples_leaf=10,
+    random_state=42
+)
 classification_tree_model.fit(X_train_c, y_train_c)
 
 y_probability = classification_tree_model.predict_proba(X_test_c)[:, 1]
-
 print(classification_tree_model.classes_)
-
 fpr, tpr, thresholds = roc_curve(y_test_c, y_probability)
 auc_metric = auc(fpr, tpr)
 

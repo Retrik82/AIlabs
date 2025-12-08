a82155baa523a6d681116d425830a2bada349c61
Retrik82 <6549701k2.0@gmail.com>
2025-12-08 16:05:42 +0300

    Added the coefficient of determination

--- lab2/main.py
+++ lab2/main.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import root_mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
@@ -54,10 +54,12 @@ y_predict_test = linear_model.predict(X_test_poly)
 MSE = mean_squared_error(y_test, y_predict_test)
 RMSE = root_mean_squared_error(y_test, y_predict_test)
 MAE = mean_absolute_error(y_test, y_predict_test)
+r_2 = r2_score(y_test, y_predict_test)
 
 print('Mean Square Error: ', MSE)
 print('Root MSE: ', RMSE)
 print('Mean Absolute Error: ', MAE)
+print('R^2: ', r_2)
 
 lasso = Lasso(alpha=0.5)
 lasso.fit(X_train, y_train)
